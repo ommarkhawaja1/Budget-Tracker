e3fe2650f83247e84d8bff38ce3750716efd424a
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-09-07 12:39:31 -0400

    linked idb script

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,7 +16,7 @@ request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
     // check if app is online, if yes run uploadTransaction() function to send all local db data to api
-    if (navigator.onLine) {
+    if (navigator.online) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
         uploadTransaction();
     }
--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,9 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
-  "./css/style.css",
+  "./css/styles.css",
   "./js/idb.js",
   "./js/index.js",
-  // "../models/transaction.js",
 ];
 
 // Respond with cached resources
